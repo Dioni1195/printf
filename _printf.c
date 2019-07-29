10ca29e1893eaf343753d8311b2af8b27804e663
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-07-29 10:14:52 -0500

    Task #0.5: create function that calls another with a specific format

---
+++ _printf.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdarg.h>
+/**
+ * _printf - function that print strings and values
+ * @format: format of value to print
+ * @...: aguments
+ * Return: count of bytes
+*/
+int _printf(const char *format, ...)
+{
+	int i = 0;
+	va_list lst;
+
+	va_start(lst, format);
+
+	if (format == '\0')
+	{
+		return (0);
+	}
+	while (format[i] != '\0')
+	{
+		if (format[i] == "%")
+		{
+			(*get_op(format[i + 1])(va_arg(lst, int)));
+		}
+		i++;
+	}
+	va_end(lst);
+	return (i);
+}
