b5229b0424d8844e1ba7a802b2f01cbda2a870e5
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-07-30 16:11:53 -0500

    update: change structure of cases function

--- _cases.c
+++ _cases.c
@@ -2,6 +2,54 @@
 #include "holberton.h"
 
 /**
+* dflt - Evaluate the cases
+* @ch: character to print
+* @lst: The variable list
+*
+* Return: A integer pointer
+*/
+
+int *dflt(const char ch, va_list lst)
+{
+	int cnt = 0, i = 0, arr[2], *ptr;
+	char *p;
+
+	ptr = arr;
+
+	switch (ch)
+	{
+	case 'c':
+		_putchar(va_arg(lst, int)), cnt++, i++;
+		break;
+	case 's':
+		p = va_arg(lst, char *), cnt += _case_s(p), i++;
+		break;
+	case '%':
+		_putchar('%'), i++, cnt++;
+		break;
+	case 'd':
+		cnt += _print_int(va_arg(lst, int)), i++;
+		break;
+	case 'i':
+		cnt += _print_int(va_arg(lst, int)), i++;
+		break;
+	case 'r':
+		cnt += _print_int(va_arg(lst, int)), i++;
+		break;
+	case 'R':
+		cnt += _print_int(va_arg(lst, int)), i++;
+		break;
+	default:
+		_putchar('%'), cnt++;
+		break;
+	}
+	i++;
+	arr[0] = cnt;
+	arr[1] = i;
+	return (ptr);
+}
+
+/**
 * cases - Evaluate the cases
 * @format: The format to print
 * @lst: The variable list
@@ -12,7 +60,7 @@
 int cases(const char *format, va_list lst)
 {
 	int i = 0, cnt = 0;
-	char *p;
+	int *p;
 
 	while (format[i] != '\0')
 	{
@@ -20,28 +68,9 @@ int cases(const char *format, va_list lst)
 			return (cnt = -1);
 		else if (format[i] == '%')
 		{
-			switch (format[i + 1])
-			{
-			case 'c':
-				_putchar(va_arg(lst, int)), cnt++, i++;
-				break;
-			case 's':
-				p = va_arg(lst, char *), cnt += _case_s(p), i++;
-				break;
-			case '%':
-				_putchar('%'), i++, cnt++;
-				break;
-			case 'd':
-				cnt += _print_int(va_arg(lst, int)), i++;
-				break;
-			case 'i':
-				cnt += _print_int(va_arg(lst, int)), i++;
-				break;
-			default:
-				_putchar(format[i]), cnt++;
-				break;
-			}
-			i++;
+			p = dflt(format[i + 1], lst);
+			i += p[1];
+			cnt += p[0];
 		}
 		else
 			_putchar(format[i]), cnt++, i++;
