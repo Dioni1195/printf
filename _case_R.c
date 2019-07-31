4d05625e2c5733f70cf0c81ce29ca27417d0b1c4
Dioni1195 <dionisio.arango95@gmail.com>
2019-07-31 02:40:09 +0000

    Fix case R : Fix malloc the copy

--- _case_R.c
+++ _case_R.c
@@ -11,25 +11,22 @@ char *_rot13(char *p)
 {
 	int i, j, c = 0;
 	char *cpy;
-	char let[52] = {'a',
-		'b', 'c', 'd', 'e', 'f', 'g', 'h',
+	char let[52] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
 		'i', 'j', 'k', 'l', 'm', 'n', 'o',
 		'p', 'q', 'r', 's', 't', 'u', 'v',
 		'w', 'x', 'y', 'z', 'A', 'B', 'C',
 		'D', 'E', 'F', 'G', 'H', 'I', 'J',
 		'K', 'L', 'M', 'N', 'O', 'P', 'Q',
-		'R', 'S', 'T', 'U', 'V', 'W', 'X',
-		'Y', 'Z'};
-	char rot[52] = {'n', 'o', 'p', 'q', 'r',
-		's', 't', 'u', 'v', 'w', 'x', 'y',
+		'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+	char rot[52] = {'n', 'o', 'p', 'q', 'r', 's',
+		't', 'u', 'v', 'w', 'x', 'y',
 		'z', 'a', 'b', 'c', 'd', 'e', 'f',
 		'g', 'h', 'i', 'j', 'k', 'l', 'm',
 		'N', 'O', 'P', 'Q', 'R', 'S', 'T',
 		'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B',
-		'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-		'K', 'L', 'M'};
+		'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'};
 
-	cpy = malloc(sizeof(char) * _str_len(p) + 1);
+	cpy = malloc(sizeof(char) * (_str_len(p) + 1));
 	if (cpy == NULL)
 		return (NULL);
 	while (p[c] != '\0')
